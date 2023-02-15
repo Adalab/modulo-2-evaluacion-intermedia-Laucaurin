9b0a86dbe88d5c5042ac96a003fd2ee5b9061006
lauracaurin <laura_cd1@hotmail.com>
2023-02-15 14:49:43 +0100

    Add correction about constant name used

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 "use strict";
 
 //Global variables
-const userNumberinput = document.querySelector('.js__user_number');
+const userNumberInput = document.querySelector('.js__user_number_input');
 const btn = document.querySelector('.js__button');
-const clue = document.querySelector('.js__clue');
-const textCounter = document.querySelector('.js__counter');
+const clueParagraph = document.querySelector('.js__clue_paragraph');
+const counterParagraph = document.querySelector('.js__counter_paragraph');
 
 
 //Counters & random number
@@ -18,21 +18,21 @@ function getRandomNumber(max) {
 
 function attemptsCounter() {
     counter++
-    textCounter.innerHTML = `Número de intentos: ${counter}`;
+    counterParagraph.innerHTML = `Número de intentos: ${counter}`;
 }
 
 function compareNumber() {
-    const userNumber = parseInt(userNumberinput.value);
+    const userNumber = parseInt(userNumberInput.value);
     if (userNumber === randomNumber) {
-        clue.innerHTML = 'Has ganado campeona!!';
+        clueParagraph.innerHTML = 'Has ganado campeona!!';
     } else if (userNumber < 1 || userNumber > 100) {
-        clue.innerHTML = 'El número debe estar entre 1 y 100';
+        clueParagraph.innerHTML = 'El número debe estar entre 1 y 100';
         attemptsCounter();
     } else if (userNumber > randomNumber) {
-        clue.innerHTML = 'Demasiado alto';
+        clueParagraph.innerHTML = 'Demasiado alto';
         attemptsCounter();
     } else if (userNumber < randomNumber) {
-        clue.innerHTML = 'Demasiado bajo';
+        clueParagraph.innerHTML = 'Demasiado bajo';
         attemptsCounter();
     }
 }
